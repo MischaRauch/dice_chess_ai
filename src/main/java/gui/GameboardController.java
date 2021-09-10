67c1819fcb879ec9537d602d1c7c52f07b193bef
LAPTOP-6B40E7JD\samgl <samglassman@protonmail.com>
2021-09-10 23:36:09 +0200

    roll

--- src/main/java/gui/GameboardController.java
+++ src/main/java/gui/GameboardController.java
@@ -1,7 +1,14 @@
 package gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import logic.Dice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameboardController {
 
@@ -9,11 +16,28 @@ public class GameboardController {
     private GridPane guiBoard;
 
     @FXML
+    private Button diceRollButton;
+
+    @FXML
+    private Label diceRoll;
+
+    @FXML
+    void roll(ActionEvent event) {
+        diceRoll.setText(Dice.roll() + "");
+    }
+
+
+    @FXML
     void initialize() {
         //create all pieces
         //set event handlers
         //can access gridpane cells using row and column indices
         //indices start at 1 since row 0 and column 0 are used to display rank and file
+        //white cells: row + col % 2 == 0
+        //black cells: row + col % 2 == 1
+        //(e.g.: in case of conditionally changing piece texture depending on background)
+
+
     }
 
 }
