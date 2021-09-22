fcbb6afbea5b077804020efbb35cec527822d33a
SimonasBubnysUM <71946857+SimonasB333@users.noreply.github.com>
2021-09-22 12:35:46 +0200

    Legal move evaluator method ideas

--- src/main/java/logic/Move.java
+++ src/main/java/logic/Move.java
@@ -6,11 +6,13 @@ public class Move {
     int startPos;
     int endPos;
     char piece;
+    Boolean isLegal; //passed as Null from GUI, returned true if move can happen,
 
-    public Move(int diceRoll, int startPos, int endPos, char piece) {
+    public Move(int diceRoll, int startPos, int endPos, char piece, Boolean isLegal) {
         this.diceRoll = diceRoll;
         this.startPos = startPos;
         this.endPos = endPos;
         this.piece = piece;
+        this.isLegal = isLegal;
     }
 }
