8a36aa45c052a4546d304c07f3b5afc68f2b687b
LAPTOP-6B40E7JD\samgl <samglassman@protonmail.com>
2021-09-22 19:11:25 +0200

    cleanup

--- src/main/java/logic/board/Board.java
+++ src/main/java/logic/board/Board.java
@@ -1,166 +1,34 @@
 package logic.board;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+public abstract class Board {
 
-public class Board implements BoardInterface {
+    public abstract boolean isEmpty(String square);
 
-    public static void main(String[] args) {
+    public abstract boolean isOffBoard(int squareNumber);
 
-        BoardInterface b = new Board(openingFEN);
-        Board afterVariousMoves = (Board) b.movePiece("e2", "e4")
-                .movePiece("b8", "c6")
-                .movePiece("b2", "b4")
-                .movePiece("c6", "b4");
+    public abstract Piece getPieceAt(String square);
 
-        System.out.println(afterVariousMoves.isFriendly("b4", Side.BLACK));
+    public abstract String getSquareAbove(String square);
+
+    public abstract String getSquareBelow(String square);
+
+    public abstract String[] getRank(int rank);
+
+    public abstract String[] getFile(String file);
+
+    public abstract String[] getDiagonals(String square);
+
+    public abstract boolean isFriendly(String square, Side side);
+
+    public boolean isFriendly(Piece piece, Side side) {
+        return piece.isFriendly(side);
     }
 
-    static Map<Integer, Integer> boardIndexMap = new HashMap<>();
+    public abstract Board movePiece(String origin, String destination);
 
-    static String tricky = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 1";
-    static String openingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 1";
+    public abstract Board loadFromFEN(String FEN);
 
-    static {
-        //populate the boardIndexMap
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                boardIndexMap.put(i * 16 + j, (112 + j) - i * 16);
-            }
-        }
-    }
+    public abstract void printBoard(boolean full);
 
-    Piece[] board;
-    String FEN;
 
-    public Board() {
-        board = new Piece[128];
-    }
-
-    public Board(Piece[] board) {
-        this.board = Arrays.copyOf(board, board.length);
-    }
-
-    public Board(String FEN) {
-        this();
-        this.FEN = FEN;
-
-        Arrays.fill(board, Piece.OFF_BOARD);
-
-        //This splits the FEN String into an array of Strings; the first 8 (0-7) indices contain
-        //the FEN encoding of that rank. The rest of the indices are not used for the board
-        String[] info = FEN.split("/|\\s"); //either split on "/" or on " " (whitespace)
-
-        for (int rank = 0; rank < 8; rank++) {
-            //turn the FEN rank String into a char array for processing
-            char[] rankSequence = info[rank].toCharArray();
-            int file = 0;
-
-            for (char c : rankSequence) {
-                if (Character.isDigit(c)) {
-                    //digits in the FEN rank sequence represent empty square offsets
-                    for (int i = 0; i < Character.getNumericValue(c); i++) {
-                        int tile = rank * 16 + (file + i);
-                        board[tile] = Piece.EMPTY;
-                    }
-                    file += Character.getNumericValue(c);
-                } else {
-                    int tile = rank * 16 + file;
-                    board[tile] = Piece.getPieceFromChar(c);
-                    file++;
-                }
-            }
-        }
-    }
-
-    public int getSquareNumber(String square) {
-        int file = ((int) square.charAt(0)) - 97; //char value of 'a' is 97
-        int rank = Character.getNumericValue(square.charAt(1)) - 1;
-        return rank * 16 + file;
-    }
-
-    public int getSquareNumber(int rank, int file) {
-        return rank * 16 + file;
-    }
-
-    @Override
-    public boolean isEmpty(String square) {
-        return getPieceAt(square) == Piece.EMPTY;
-    }
-
-    @Override
-    public boolean isOffBoard(int squareNumber) {
-        return (squareNumber & 0x88) != 0;
-    }
-
-    @Override
-    public Piece getPieceAt(String square) {
-        return board[boardIndexMap.get(getSquareNumber(square))];
-    }
-
-    @Override
-    public String getSquareAbove(String square) {
-        return null;
-    }
-
-    @Override
-    public String getSquareBelow(String square) {
-        return null;
-    }
-
-    @Override
-    public String[] getRank(int rank) {
-        return new String[0];
-    }
-
-    @Override
-    public String[] getFile(String file) {
-        return new String[0];
-    }
-
-    @Override
-    public String[] getDiagonals(String square) {
-        return new String[0];
-    }
-
-    @Override
-    public boolean isFriendly(String square, Side side) {
-        return board[boardIndexMap.get(getSquareNumber(square))].isFriendly(side);
-    }
-
-    @Override
-    public BoardInterface movePiece(String origin, String destination) {
-        int originSquare = getSquareNumber(origin);
-        int destinationSquare = getSquareNumber(destination);
-
-        Board result = new Board(board);
-
-        result.board[boardIndexMap.get(destinationSquare)] = result.board[boardIndexMap.get(originSquare)];
-        result.board[boardIndexMap.get(originSquare)] = Piece.EMPTY;
-
-        result.printBoard(false);
-
-        return result;
-    }
-
-    @Override
-    public BoardInterface loadFromFEN(String FEN) {
-        return new Board(FEN);
-    }
-
-    public void printBoard(boolean full) {
-        final int MAX_FILE = full ? 16 : 8;
-        String files = "　　　A　B　C　D　E　F　G　H  \n";
-        System.out.println(files);
-        for (int rank = 0; rank < 8; rank++) {
-            System.out.print(" " + (8 - rank) + " 　");
-            for (int file = 0; file < MAX_FILE; file++) {
-                int tile = rank * 16 + file; // this converts to board index
-                System.out.print(board[tile].getType() + " ");
-            }
-            System.out.println("　"+ (8 - rank) + " ");
-        }
-        System.out.println("\n"+files + "\n\n");
-    }
 }
--- src/main/java/logic/board/Board0x88.java
+++ src/main/java/logic/board/Board0x88.java
@@ -4,78 +4,152 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Board0x88 {
+public class Board0x88 extends Board {
 
-    static String openingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 1";
-    static String tricky = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 1";
+    public static void main(String[] args) {
+
+        Board b = new Board0x88(openingFEN);
+        Board0x88 afterVariousMoves = (Board0x88) b.movePiece("e2", "e4")
+                .movePiece("b8", "c6")
+                .movePiece("b2", "b4")
+                .movePiece("c6", "b4");
+
+        System.out.println(afterVariousMoves.isFriendly("b4", Side.BLACK));
+    }
 
     static Map<Integer, Integer> boardIndexMap = new HashMap<>();
-    static Map<Character, Piece> charPieceMap = new HashMap<>();
-    static {
-        charPieceMap.put('P', Piece.WHITE_PAWN);
-        charPieceMap.put('N', Piece.WHITE_KNIGHT);
-        charPieceMap.put('B', Piece.WHITE_BISHOP);
-        charPieceMap.put('R', Piece.WHITE_ROOK);
-        charPieceMap.put('Q', Piece.WHITE_QUEEN);
-        charPieceMap.put('K', Piece.WHITE_KING);
-        charPieceMap.put('p', Piece.BLACK_PAWN);
-        charPieceMap.put('n', Piece.BLACK_KNIGHT);
-        charPieceMap.put('b', Piece.BLACK_BISHOP);
-        charPieceMap.put('r', Piece.BLACK_ROOK);
-        charPieceMap.put('q', Piece.BLACK_QUEEN);
-        charPieceMap.put('k', Piece.BLACK_KING);
-        charPieceMap.put('o', Piece.OFF_BOARD);
 
+    static String tricky = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 1";
+    static String openingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 1";
+
+    static {
+        //populate the boardIndexMap
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 boardIndexMap.put(i * 16 + j, (112 + j) - i * 16);
             }
         }
-
     }
 
     Piece[] board;
     String FEN;
 
-    public Board0x88(String FEN) {
-        board = new Piece[128];
-        this.FEN = FEN;
-        loadFromFEN(FEN);
-    }
-
     public Board0x88() {
-        this(openingFEN);
+        board = new Piece[128];
     }
 
-    public void movePiece(String origin, String destination) {
-        //format like file + rank (e.g.: b6, e4, etc)
-        int originFile = ((int) origin.charAt(0)) - 97; //char value of 'a' is 97
-        int originRank = Character.getNumericValue(origin.charAt(1)) - 1;
-        int originBoardIndex = originRank * 16 + originFile;
-
-        int destFile = ((int) destination.charAt(0)) - 97;; //char value of 'a' is 97
-        int destRank = Character.getNumericValue(destination.charAt(1)) - 1;
-        int destBoardIndex = destRank * 16 + destFile;
-
-        board[boardIndexMap.get(destBoardIndex)] = board[boardIndexMap.get(originBoardIndex)];
-        board[boardIndexMap.get(originBoardIndex)] = Piece.EMPTY;
-
-        printBoard(false);
+    public Board0x88(Piece[] board) {
+        this.board = Arrays.copyOf(board, board.length);
     }
 
-    public void createEmptyBoard() {
+    public Board0x88(String FEN) {
+        this();
+        this.FEN = FEN;
+
         Arrays.fill(board, Piece.OFF_BOARD);
 
+        //This splits the FEN String into an array of Strings; the first 8 (0-7) indices contain
+        //the FEN encoding of that rank. The rest of the indices are not used for the board
+        String[] info = FEN.split("/|\\s"); //either split on "/" or on " " (whitespace)
+
         for (int rank = 0; rank < 8; rank++) {
-            for (int file = 0; file < 16; file++) {
-                int tile = rank * 16 + file;
-                if ((tile & 0x88) == 0) {
-                    board[tile] = Piece.EMPTY;
+            //turn the FEN rank String into a char array for processing
+            char[] rankSequence = info[rank].toCharArray();
+            int file = 0;
+
+            for (char c : rankSequence) {
+                if (Character.isDigit(c)) {
+                    //digits in the FEN rank sequence represent empty square offsets
+                    for (int i = 0; i < Character.getNumericValue(c); i++) {
+                        int tile = rank * 16 + (file + i);
+                        board[tile] = Piece.EMPTY;
+                    }
+                    file += Character.getNumericValue(c);
+                } else {
+                    int tile = rank * 16 + file;
+                    board[tile] = Piece.getPieceFromChar(c);
+                    file++;
                 }
             }
         }
     }
 
+    public int getSquareNumber(String square) {
+        int file = ((int) square.charAt(0)) - 97; //char value of 'a' is 97
+        int rank = Character.getNumericValue(square.charAt(1)) - 1;
+        return rank * 16 + file;
+    }
+
+    public int getSquareNumber(int rank, int file) {
+        return rank * 16 + file;
+    }
+
+    @Override
+    public boolean isEmpty(String square) {
+        return getPieceAt(square) == Piece.EMPTY;
+    }
+
+    @Override
+    public boolean isOffBoard(int squareNumber) {
+        return (squareNumber & 0x88) != 0;
+    }
+
+    @Override
+    public Piece getPieceAt(String square) {
+        return board[boardIndexMap.get(getSquareNumber(square))];
+    }
+
+    @Override
+    public String getSquareAbove(String square) {
+        return null;
+    }
+
+    @Override
+    public String getSquareBelow(String square) {
+        return null;
+    }
+
+    @Override
+    public String[] getRank(int rank) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] getFile(String file) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] getDiagonals(String square) {
+        return new String[0];
+    }
+
+    @Override
+    public boolean isFriendly(String square, Side side) {
+        return board[boardIndexMap.get(getSquareNumber(square))].isFriendly(side);
+    }
+
+    @Override
+    public Board movePiece(String origin, String destination) {
+        int originSquare = getSquareNumber(origin);
+        int destinationSquare = getSquareNumber(destination);
+
+        Board0x88 result = new Board0x88(board);
+
+        result.board[boardIndexMap.get(destinationSquare)] = result.board[boardIndexMap.get(originSquare)];
+        result.board[boardIndexMap.get(originSquare)] = Piece.EMPTY;
+
+        result.printBoard(false);
+
+        return result;
+    }
+
+    @Override
+    public Board loadFromFEN(String FEN) {
+        return new Board0x88(FEN);
+    }
+
+    @Override
     public void printBoard(boolean full) {
         final int MAX_FILE = full ? 16 : 8;
         String files = "　　　A　B　C　D　E　F　G　H  \n";
@@ -90,27 +164,4 @@ public class Board0x88 {
         }
         System.out.println("\n"+files + "\n\n");
     }
-
-
-
-    public void loadFromFEN(String FEN) {
-        String[] info = FEN.split("/|\\s"); //either split on "/" or on " " (whitespace)
-
-        for (int rank = 0; rank < 8; rank++) {
-            char[] rankSequence = info[rank].toCharArray();
-            int file = 0;
-            for (char c : rankSequence) {
-                if (Character.isDigit(c)) {
-                    file += Character.getNumericValue(c);
-                } else {
-                    int tile = rank * 16 + file;
-                    if ((tile & 0x88) == 0) {
-                        board[tile] = charPieceMap.get(c);
-                    }
-                    file++;
-                }
-            }
-        }
-    }
-
 }
