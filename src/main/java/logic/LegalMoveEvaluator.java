1a73bbeb11fa6dc048786f3e50ab8f9b8946889c
LAPTOP-6B40E7JD\samgl <samglassman@protonmail.com>
2021-09-24 03:48:41 +0200

    i should probably sleep

--- src/main/java/logic/LegalMoveEvaluator.java
+++ src/main/java/logic/LegalMoveEvaluator.java
@@ -12,13 +12,37 @@ public class LegalMoveEvaluator {
     public boolean isLegalMove(Move move, State state) {
         Board board = state.board;
         if (move.getPiece() == Piece.WHITE_PAWN) {
-            Square above = board.getSquareAbove(move.getOrigin());
-            if (board.isEmpty(above)) {
-                //nlbbalsblabalb
-            }
+            return isLegalPawnMove(move, state);
 
         }
 
+        return true;
+    }
+
+    //idk
+    public boolean isLegalPawnMove(Move move,State state) {
+         Board b = state.board;
+
+         if (move.getColor() == Side.WHITE) {
+             if (b.getPieceAt(move.origin) != Piece.WHITE_PAWN) {
+                 //user chose wrong piece/piece color to move -> invalid
+                 return false;
+             } else {
+                 if (move.getOrigin().getSquareAbove() == move.getDestination()) {
+                     //the pawn wanted a single move forward
+                     //if that square is occupied by friendly piece and we cant go there
+                     //or the piece is an empty square or its an opponent, in which case you can capture
+                     return !b.getPieceAt(move.getDestination()).isFriendly(move.getColor());
+                 }
+             }
+         }
+
+
+
+
+         //if pawn is on second rank then can jump 2 squares
+            ///TODO: method which returns the rank of the Piece
+        //else just one
         return true;
     }
 
