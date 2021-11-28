de25dfed340581f02dc1f0f6f42c568fa3ba2707
sanchi bhalla <sanchibhalla23@gmail.com>
2021-11-28 12:28:19 +0100

    added QL player to AI

--- src/main/java/logic/player/QLPlayer.java
+++ src/main/java/logic/player/QLPlayer.java
@@ -1,9 +1,14 @@
 package logic.player;
 
 import logic.ML.DQL;
+import logic.ML.Qtable;
 import logic.Move;
 import logic.State;
 import logic.enums.Side;
+import logic.game.Game;
+
+
+import java.util.List;
 
 public class QLPlayer extends AIPlayer{
     private final int depth;
@@ -14,8 +19,38 @@ public class QLPlayer extends AIPlayer{
         this.depth = depth;
     }
 
+    Qtable currentQtable;
+    Game game = Game.getInstance();
+    State initialState = game.getCurrentState();
+    int Stateindex = currentQtable.accessStateIndex(initialState);
+    double[][] Qvalues;
+
+
     @Override
-    public Move chooseMove(State state) { // TODO, implement this
-       return null;
-    }
+    public Move chooseMove(State state){ // TODO, implement this
+
+        List<Move> validMoves = getValidMoves(state);
+
+        ql.algo(color,depth);
+        double maxvalue = MaxvalueforState(initialState);
+
+        int MoveMaxIndex = ql.argmax(Qvalues,Stateindex);
+        Move chosenMove = validMoves.get(MoveMaxIndex);  // choose the move with the maximum value for the state
+        return chosenMove;
+
+        }
+
+
+        public double MaxvalueforState(State state)
+        {
+
+            double value = ql.maxValue(Qvalues,Stateindex);
+            return value;
+
+        }
+
+
+
+
+
 }
